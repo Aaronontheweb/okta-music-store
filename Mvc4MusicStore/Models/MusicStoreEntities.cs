43fbb66a0269c53bec88645f36af3eba73d5a12e
Joël Franusic <joel.franusic@okta.com>
2014-11-10 12:29:33 -0800

    Part 8: Shopping Cart with Ajax Updates

--- Mvc4MusicStore/Models/MusicStoreEntities.cs
+++ Mvc4MusicStore/Models/MusicStoreEntities.cs
@@ -10,7 +10,10 @@ namespace Mvc4MusicStore.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
-
         public DbSet<Artist> Artists { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
---
+++ Mvc4MusicStore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Mvc4MusicStore.Models;
+
+namespace Mvc4MusicStore.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
