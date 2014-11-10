2ca4a0757cbc7ce162fa9fe36216244d57bb2b93
Joël Franusic <joel.franusic@okta.com>
2014-11-10 12:24:59 -0800

    Part 2: Controllers

--- Mvc4MusicStore/Controllers/HomeController.cs
+++ Mvc4MusicStore/Controllers/HomeController.cs
@@ -8,25 +8,9 @@ namespace Mvc4MusicStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return "Hello from Home";
         }
     }
 }
