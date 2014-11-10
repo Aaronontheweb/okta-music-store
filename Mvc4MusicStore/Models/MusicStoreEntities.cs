360df94c3c13e73c033bf689609a0181f3a787ca
Joël Franusic <joel.franusic@okta.com>
2014-11-10 12:27:26 -0800

    Part 5: Edit Forms and Templating

--- Mvc4MusicStore/Models/MusicStoreEntities.cs
+++ Mvc4MusicStore/Models/MusicStoreEntities.cs
@@ -10,5 +10,7 @@ namespace Mvc4MusicStore.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
     }
 }
